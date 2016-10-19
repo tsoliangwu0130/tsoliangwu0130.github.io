4cca3c390bb6f50df8e1a65bd45c8e5418f2967d
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-10-18 22:05:15 -0700

    Fix bugs

--- assets/js/search_filter.js
+++ assets/js/search_filter.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
+	console.log("ob­ject-ori­ented pro­gram­ming ba­sics".indexOf("obj"));
 	$("#filter_box").keyup(function(){
 		var value = $(this).val().toLowerCase();
 		$(".inner li").each(function(index, element){
-			console.log($(element).text());
 			if ($(element).text().toLowerCase().indexOf(value) == -1){
 				$(element).hide();
 			} else{
+				console.log($(element).text().toLowerCase());
 				$(element).show();
 			};
 		});
+
+		// $(".inner ul").each(function(index, element){
+		// 	if ($(element).children(":visible").length == 0){
+		// 		$(element).hide();
+		// 	} else {
+		// 		$(element).show();
+		// 	};
+		// });
+
+		// $(".inner > ul").each(function(index, element){
+		// 	if ($(element).children(":visible").length == 0){
+		// 		$(element).closest("h3").hide();
+		// 	} else {
+		// 		$(element).closest("h3").show();
+		// 	};	
+		// });
 	});
 });
