cb6744ae123ea02b4a87d8d9de56434d59a81603
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-10-18 22:38:04 -0700

    Update filter

--- assets/js/search_filter.js
+++ assets/js/search_filter.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	console.log("ob­ject-ori­ented pro­gram­ming ba­sics".indexOf("obj"));
 	$("#filter_box").keyup(function(){
 		var value = $(this).val().toLowerCase();
 		$(".inner li").each(function(index, element){
@@ -10,21 +9,5 @@ $(document).ready(function(){
 				$(element).show();
 			};
 		});
-
-		// $(".inner ul").each(function(index, element){
-		// 	if ($(element).children(":visible").length == 0){
-		// 		$(element).hide();
-		// 	} else {
-		// 		$(element).show();
-		// 	};
-		// });
-
-		// $(".inner > ul").each(function(index, element){
-		// 	if ($(element).children(":visible").length == 0){
-		// 		$(element).closest("h3").hide();
-		// 	} else {
-		// 		$(element).closest("h3").show();
-		// 	};	
-		// });
 	});
 });
