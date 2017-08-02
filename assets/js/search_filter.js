343121adf3439b0f6189ea64c0e9f08440ca7c45
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-08-02 14:11:37 -0700

    Update code style

--- assets/js/search_filter.js
+++ assets/js/search_filter.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-	$("#filter_box").keyup(function(){
+$(document).ready(function() {
+	$('#filter_box').keyup(function() {
 		var value = $(this).val().toLowerCase();
-		$(".item li").each(function(index, element){
-			if ($(element).text().toLowerCase().indexOf(value) == -1){
+		$('.item li').each(function(index, element) {
+			if ($(element).text().toLowerCase().indexOf(value) == -1) {
 				$(element).hide();
-			} else{
+			} else {
 				$(element).show();
 			};
 		});
