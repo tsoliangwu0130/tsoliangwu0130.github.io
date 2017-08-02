ea82f9706ada69e632f8760a52606c40b12c0d7e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-08-02 13:57:31 -0700

    Fix element selector bug

--- assets/js/search_filter.js
+++ assets/js/search_filter.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 	$("#filter_box").keyup(function(){
 		var value = $(this).val().toLowerCase();
-		$(".inner li").each(function(index, element){
+		$(".item li").each(function(index, element){
 			if ($(element).text().toLowerCase().indexOf(value) == -1){
 				$(element).hide();
 			} else{
-				console.log($(element).text().toLowerCase());
 				$(element).show();
 			};
 		});
