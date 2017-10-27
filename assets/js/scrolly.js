3353ddaf81af23f147d2e725a090cc529a6d8567
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-10-27 13:45:54 -0700

    Finsih mouse

---
+++ assets/js/scrolly.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#mouse").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 1000,
+            easing: "swing"
+        });
+        return false;
+    });
+});
