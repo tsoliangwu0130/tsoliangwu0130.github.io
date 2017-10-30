9447ab6ee6e6ae7614a6b382465cfe6a76abc516
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-10-30 15:10:31 -0700

    Add some notes

--- assets/js/tocify.js
+++ assets/js/tocify.js
@@ -1,10 +1,12 @@
+// See tocify documentation here: http://gregfranko.com/jquery.tocify.js
 $(document).ready(function(){
     $("#toc").tocify({
         selectors: "h2, h3",
-        scrollTo: 70
+        scrollTo: 60
     });
 });
 
+// Adjust and fix toc position
 $(document).scroll(function(e) {
     if($(this).scrollTop() > $("#header").height()) {
         $("#toc").css({"position": "fixed", "top": "10%"});
