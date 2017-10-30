b9ffd93925ed7a19b0e4cf87028adb7e65d5a57d
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-10-30 14:18:52 -0700

    Fix anchor issue

--- assets/js/tocify.js
+++ assets/js/tocify.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
     $("#toc").tocify({
-        selectors: "h2, h3"
+        selectors: "h2, h3",
+        scrollTo: 70
     });
 });
 
 $(document).scroll(function(e) {
     if($(this).scrollTop() > $("#header").height()) {
-        $("#toc").css({"position" : "fixed", "top" : "10%"});
+        $("#toc").css({"position": "fixed", "top": "10%"});
     }
     else {
-        $("#toc").css({"position" : "absolute", "top": "0"});
+        $("#toc").css({"position": "absolute", "top": "0"});
     }
 });
